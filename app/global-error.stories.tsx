92c8384e3df1c3579c03b73dade746418b245676
Iurii Cherkashin <cherkashin.ju@gmail.com>
2026-09-18 18:18:38 -0300

    refactor: remove unused error digest file and update imports in error stories to reference new location

--- app/global-error.stories.tsx
+++ app/global-error.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from "@storybook/nextjs-vite";
 
 import { fn } from "storybook/test";
 
-import { EXAMPLE_ERROR_DIGEST } from "@/.storybook/error-digest";
+import { EXAMPLE_ERROR_DIGEST } from "@/features/error-widget/consts";
 import { routing } from "@/i18n/routing";
 
 import GlobalError from "./global-error";
--- features/error-widget/error-widget.stories.tsx
+++ features/error-widget/error-widget.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from "@storybook/nextjs-vite";
 
 import { fn } from "storybook/test";
 
-import { EXAMPLE_ERROR_DIGEST } from "@/.storybook/error-digest";
+import { EXAMPLE_ERROR_DIGEST } from "./consts";
 
 import { ErrorWidget } from "./error-widget";
 
